b7e20dfc688f513946ffb8069773063b4d430e94
BuggedPotato <donko812@gmail.com>
2026-05-28 20:05:09 +0200

    initial commit

---
+++ src/main/java/pl/edu/pk/klockowisko/KlockowiskoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.klockowisko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlockowiskoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlockowiskoApplication.class, args);
+    }
+
+}
